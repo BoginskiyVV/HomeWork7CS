1656a3530d833e0c6bbb26761e8e36eb3d9ba5dc
BoginskiyVV <vov4ik84@gmail.com>
2022-09-19 01:39:38 +0300

    Задача № 50

---
+++ Task50/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 50. 
+// Напишите программу, которая на вход принимает число и ищет в двумерном массиве, 
+// и возвращает индексы этого элемента или же указание, что такого элемента нет.
+
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Convert.ToInt32(new Random().Next(0, 15));
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t  ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+FindIndexNumber(array);
+
+int FindIndexNumber(int[,] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (Number == array[i, j])
+                Console.WriteLine($"Индекс числа {Number} в массиве => [{i},{j}]");
+        }
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (Number == array[i, j])
+                counter++;
+        }
+    }
+    // Console.WriteLine(counter);
+    if (counter == 0)
+        Console.WriteLine($"Число {Number} не в массиве");
+    return counter;
+}
+
+
+
+
+
